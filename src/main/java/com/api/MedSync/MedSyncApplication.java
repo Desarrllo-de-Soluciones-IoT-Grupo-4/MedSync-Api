a1b0149b7d2642944fed36cae8bbfe83ee7805a6
William062004 <U202117904@upc.edu.pe>
2024-10-31 04:52:27 -0500

    CORS Configuration

--- src/main/java/com/api/MedSync/MedSyncApplication.java
+++ src/main/java/com/api/MedSync/MedSyncApplication.java
@@ -1,8 +1,16 @@
 package com.api.MedSync;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@OpenAPIDefinition(
+		info = @Info(title = "MedSync API", version = "v1"),
+		servers = {
+				@Server(url = "https://medsync-api.up.railway.app", description = "Railway API Server")
+		}
+)
 @SpringBootApplication
 public class MedSyncApplication {
 
